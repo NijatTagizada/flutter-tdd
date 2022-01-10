a5454ce053f174a7b86f40dcabe3c95c78228867
Nijat <tagizada.nicat@gmail.com>
2022-01-10 17:43:35 +0400

    added bloc test

--- lib/presentation/bloc/number_trivia/number_trivia_cubit.dart
+++ lib/presentation/bloc/number_trivia/number_trivia_cubit.dart
@@ -1,5 +1,8 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+
+import '../../../domain/entities/number_trivia.dart';
 
 part 'number_trivia_state.dart';
 
